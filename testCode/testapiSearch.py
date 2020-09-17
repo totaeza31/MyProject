8195b18c4c7ac846d71a50ae029f232478f7b69a
toteza31@gmail.com <toteza31@gmail.com>
2020-09-17 19:53:02 +0700

    3

--- testCode/testapiSearch.py
+++ testCode/testapiSearch.py
@@ -1,7 +1,6 @@
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource ,output_json
 from flask import Flask , jsonify
-import sys
-import settings
+from flask.ext.restful import reqparse, abort
 import json
 
 app = Flask(__name__)
@@ -9,16 +8,14 @@ api = Api(app)
 
 class Helloword(Resource):
     def get(self,name):
-        url = './testCode/json/UniquewordDeepcutWordTestsetNomal.json'
+        url = './testCode/json/UniquewordDeepcutWordTestsetNomal.json'     
         with open(url,encoding="utf-8") as f: 
           names = json.load(f)
           # obj = names[name]
-        return  names[name]
+        return  make_response(names[name])
         # return names[name]
 api.add_resource(Helloword,"/helloword/<string:name>")
 
 
 if __name__ == "__main__":
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
     app.run(debug=True)
